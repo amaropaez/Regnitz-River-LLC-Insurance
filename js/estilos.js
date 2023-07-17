69c93c2570d21e90f5b1df2e25ab6ea845c4a88b
Nadia Amaro <amaropaez@gmail.com>
2023-07-17 10:37:35 -0300

    g

--- js/estilos.js
+++ js/estilos.js
@@ -1,15 +1,7 @@
-document.getElementById("icon_menu").addEventListener("click", mostrar_menu);
-
-function mostrar_menu(){
-
-    document.querySelector(".menu").classList.toggle("mostrar_menu");
-    
-}
-
-window.onscroll = function () {
-    var posicion = window.pageYOffset || document.documentElement.scrollTop;
-    var elemento1 = document.getElementById("icon_heart");
-    var elemento2 = document.getElementById("icon_fire");
-    elemento1.style.bottom = posicion * 0.1 + "px";
-    elemento2.style.top = posicion * 0.1 + "px";
+function cambiarClase(){
+    let siteNav = document.getElementById('site-nav');
+        siteNav.classList.toggle('site-nav-open');
+    let menuOpen = document.getElementById('menu-toggle');
+        menuOpen.classList.toggle('menu-open');    
+        
 }
